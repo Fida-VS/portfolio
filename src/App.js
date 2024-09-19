cb005fba74d03c2a5055d0c370c5e0c395af1059
Fida-VS <poletaeva69vera@mail.ru>
2024-09-19 11:18:39 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Main } from './pages/main/main';
+
+
+function App() {
+  return (
+    <Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="*" element={<div>Страница с ошибкой</div>} />
+				</Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/main/main.js
@@ -0,0 +1,10 @@
+import styles from './main.module.css';
+
+export const Main = () => {
+
+
+
+    return (
+        <div className={styles.main}>Главная</div>
+    );
+}
