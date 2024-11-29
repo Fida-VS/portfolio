381ba735678582e086a635a723be3f8126a585d3
Fida-VS <poletaeva69vera@mail.ru>
2024-11-29 12:08:57 +0300

    corrects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import { Main } from "./pages/main/main";
-import { useEffect } from "react";
+
 
 function App() {
-
-  useEffect(() => {
-    const ratio = window.devicePixelRatio;
-  
-    if (ratio > 1) {
-      console.log("Масштаб увеличен");
-  
-      document.body.style.transform = 'scale(1)';
-  }
-  }, [])
 
   return (
     <Routes>
