ab29f48856631c1d70d436c011377805379d9b81
Fida-VS <poletaeva69vera@mail.ru>
2024-11-29 12:03:55 +0300

    corrects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Main } from "./pages/main/main";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    const ratio = window.devicePixelRatio;
+  
+    if (ratio > 1) {
+      console.log("Масштаб увеличен");
+  
+      document.body.style.transform = 'scale(1)';
+  }
+  }, [])
+
   return (
     <Routes>
       <Route path="/portfolio" element={<Main />} />
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,8 +1,10 @@
+
 import { Content } from "../../components/content/content";
 import { Sidebar } from "../../components/sidebar/sidebar";
 import styles from "./main.module.css";
 
 export const Main = () => {
+
   return (
     <div className={styles.main}>
       <Sidebar />
